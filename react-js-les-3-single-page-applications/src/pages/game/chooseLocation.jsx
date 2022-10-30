b7ebc226adb3a2ba8f0b0ab6598b1322c181e1cd
fofokseek <dennis.nijs@hotmail.com>
2022-10-30 10:11:30 +0100

    updated game, chooselocation - finished exercise

--- react-js-les-3-single-page-applications/src/pages/game/chooseLocation.jsx
+++ react-js-les-3-single-page-applications/src/pages/game/chooseLocation.jsx
@@ -1,7 +1,26 @@
+import {ListGroup} from "react-bootstrap";
+import {getAllLocations, getSelectedLocation, setSelectedLocation} from "../../API/capitalsAPI.js";
+import {useNavigate} from "react-router-dom";
+
 const ChooseLocation = () => {
+    const selectedLocation = getSelectedLocation()
+    const locations = getAllLocations()
+    const navigate = useNavigate()
+
+    const selectLocation = (location) => {
+        setSelectedLocation(location)
+        navigate(`/game/play/${location}`)
+    }
+
     return (
-        <>
-        </>
+        <ListGroup as="ul">
+            {locations.map(l => (
+                <ListGroup.Item key={l} as="li" active={selectedLocation === l} action
+                                onClick={() => selectLocation(l)}>
+                    {l}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
     )
 }
 
--- react-js-les-3-single-page-applications/src/pages/game/game.jsx
+++ react-js-les-3-single-page-applications/src/pages/game/game.jsx
@@ -1,6 +1,31 @@
+import {getSelectedLocation} from "../../API/capitalsAPI.js";
+import {Link, Navigate, Outlet, useLocation} from "react-router-dom";
+
 const Game = () => {
+    const chosenLocation = getSelectedLocation()
+    const {pathname} = useLocation()
+
+    let text
+    if (chosenLocation) {
+        text = (
+            <p>
+                You've chosen to practice the capitals in {chosenLocation}. <Link to={'location'}>Click
+                here</Link>&nbsp; to change the location.
+            </p>
+        )
+    } else {
+        text = <p> Please choose a location before playing the game.</p>
+    }
+
+    if (!chosenLocation && !pathname.endsWith('location')) {
+        return <Navigate to={'location'}/>
+    }
+
     return (
         <>
+            <h1>Play the game!</h1>
+            {text}
+            <Outlet/>
         </>
     )
 }
